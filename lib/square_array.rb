38b80d2b1a6cbca95ab58953b00aaf6ae376cd45
Yuriy Berezskyy <bereza95@gmail.com>
2019-09-10 17:51:15 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,12 +2,9 @@ def square_array(array)
     count = 0 
     arr = []
      while count < array.length do 
-      puts arr = array[count] ** 2
+      arr[count] = array[count] ** 2
       count+=1
      end
-     while count < arr.length do
-       count+=1 
-     end
-    
+   
   end
 
